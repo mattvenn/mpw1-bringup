b815a0da87bf9401f3a75217c4472d46471e741b
Matt Venn <matt@mattvenn.net>
2023-05-17 17:21:31 +0200

    note on fix

--- vga.sh
+++ vga.sh
@@ -1,3 +1,4 @@
 export CROSS=riscv64-unknown-elf-
 port=/dev/serial/by-id/usb-FTDI_Dual_RS232-HS-if01-port0
-./sw/control.py --port $port --vdd 375 --vdd1 440 --vdd2 375 vga
+# was broken - fixed by changing --vdd from 375 to 370 on Wed 17 May 17:21:08 CEST 2023
+./sw/control.py --port $port --vdd 370 --vdd1 440 --vdd2 375 vga
